7b21c6d7fd98fe91a304492504e553ba41c10c17
Lyndon <lyndonrenaud98@protonmail.com>
2020-01-15 11:26:02 -0500

    reinit files

---
+++ app.js
@@ -0,0 +1,40 @@
+const spawn = require('child_process').spawn;
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'ejs'); //set view engine to EJS
+app.use('assets', express.static('assets')); //set static folder
+
+app.get('/', async function(req, res){
+  var disks, temp, ram_usage;
+    
+  try{
+    //get storage usage
+    disks = await spawncmd('df -h | head -n1');
+
+    //get cpu temperature
+    temps = await spawncmd('/opt/vc/bin/vcgencmd measure_temp');
+
+    //get free memory
+    ram_usage = await spawncmd('free -m);
+  }
+  catch(error){
+    console.log("promise error: " + error);
+  }
+  res.render('index', {disks: disks, temps: temps});
+})
+
+app.listen(9999);
+
+//input: bash command string
+//returns a promise containing the bash command output
+function spawncmd(cmd){
+  return new Promise(function(resolve, reject){
+    var proc = spawn('sh', ['-c', cmd]);
+    proc.stdout.on('data', function(data){
+      console.log('running proc');
+      console.log('my data: ' + data);
+      resolve(data);
+    });
+  });
+}
